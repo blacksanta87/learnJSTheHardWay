2b549bfb2f199cf31c8a175e98fbd4f91b361e3e
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 19:45:01 +0800

    Learn more about printing and escape character

---
+++ ex9.js
@@ -0,0 +1,14 @@
+// Here's some new strange stuff, remember type it exactly.
+
+let days = 'Mon Tue Wed Thu Fri Sat Sun';
+let months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug';
+
+console.log(`Here are the days: ${days}`);
+console.log(`Here are the months: ${months}`);
+
+console.log(`
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+`);
