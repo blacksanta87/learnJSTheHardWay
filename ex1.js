3d80380989a6236a0c16efed36c4601ff826656a
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 17:38:43 +0800

    Print statement to console

---
+++ ex1.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+console.log('Hello Again');
+console.log('I like typing this.');
+console.log('This is fun.');
+console.log('Yay! Printing.');
+console.log("I'd much rather you 'not'.");
+console.log('I "said" do not touch this.');
