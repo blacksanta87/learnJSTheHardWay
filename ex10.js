8d05b6d177d5e1d75d9b95f3ee6f1902be46b19b
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 19:49:05 +0800

    Escape character in template literal

---
+++ ex10.js
@@ -0,0 +1,15 @@
+let tabbyCat = "\tI'm tabbed in.";
+let persianCat = "I'm split\non a line.";
+let backlashCat = "I'm \\ a \\ cat.";
+
+let fatCat = `
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+`;
+
+console.log(tabbyCat);
+console.log(persianCat);
+console.log(backlashCat);
+console.log(fatCat);
