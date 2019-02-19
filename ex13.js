a24737198efda913f183cdf9336c6df4b3fc24a7
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 20:52:59 +0800

    Learn about arguments variable

---
+++ ex13.js
@@ -0,0 +1,8 @@
+let first, second, third;
+
+[first, second, third] = process.argv.slice(2);
+
+console.log(`Your first variable is: ${first}`);
+console.log(`Your second variable is: ${second}`);
+console.log(`Your third variable is: ${third}`);
+
