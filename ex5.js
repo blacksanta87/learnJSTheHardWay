59959e2213e0d70936c5ceeff6f0d2f6b9098ad1
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 18:12:07 +0800

    More variable and template literals

---
+++ ex5.js
@@ -0,0 +1,17 @@
+let myName = 'Zed A. Shaw';
+let myAge = 35; // not a lie in 2009
+let myHeight = 74; // inches
+let myWeight = 180; // lbs
+let myEyes = 'Blue';
+let myTeeth = 'White';
+let myHair = 'Brown';
+
+console.log(`Let's talk about ${myName}.`);
+console.log(`He's ${myHeight} inches tall.`);
+console.log(`He's ${myWeight} pounds heavy.`);
+console.log(`Actually that's not too heavy.`);
+console.log(`He's got ${myEyes} eyes and ${myHair} hair.`);
+console.log(`His teeth are usually ${myTeeth} depending on the coffee.`);
+
+// this line is tricky, try to get it exactly right
+console.log(`If I add ${myAge}, ${myHeight}, and ${myWeight} I get ${myAge + myHeight + myWeight}.`);
