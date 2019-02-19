8ef02befc355e9260265bea3d4576125741f735e
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 18:28:44 +0800

    Use repeat method on string

---
+++ ex7.js
@@ -0,0 +1,19 @@
+console.log('Mary had a little lamb.');
+console.log(`Its fleece was white as ${'snow'}.`);
+console.log('And everywhere that Mary went.');
+console.log('.'.repeat(10));
+
+let end1 = 'C';
+let end2 = 'h';
+let end3 = 'e';
+let end4 = 'e';
+let end5 = 's';
+let end6 = 'e';
+let end7 = 'B';
+let end8 = 'u';
+let end9 = 'r';
+let end10 = 'g';
+let end11 = 'e';
+let end12 = 'r';
+
+console.log(`${end1 + end2 + end3 + end4 + end5 + end6}${end7 + end8 + end9 + end10 + end11 + end12}`);
