bdbd6f897f8c096966a04047dcbff291c1299147
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-19 18:18:34 +0800

    Learn string and text

---
+++ ex6.js
@@ -0,0 +1,21 @@
+let typesOfPeople = 10;
+let x = `There are ${typesOfPeople} types of people.`;
+let binary = 'binary';
+let doNot = "don't";
+let y = `Those who know ${binary} and those who ${doNot}.`;
+
+console.log(x);
+console.log(y);
+
+console.log(`I said: ${x}.`);
+console.log(`I also said: '${y}'.`);
+
+let hilarious = false;
+let jokeEvaluation = `Isn't that joke so funny?! ${hilarious}`;
+
+console.log(jokeEvaluation);
+
+let w = 'This is the left side of...';
+let e = 'a string with a right side.';
+
+console.log(`${w}${e}`);
